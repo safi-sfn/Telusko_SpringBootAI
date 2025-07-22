bb241e676012704c94a06e72c318ca33622aebf7
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-22 13:13:21 +0530

    initial commit

---
+++ project1/src/main/java/com/safi/project1/Project1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.safi.project1;
+
+/**
+ *
+ * @author safi
+ */
+public class Project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
