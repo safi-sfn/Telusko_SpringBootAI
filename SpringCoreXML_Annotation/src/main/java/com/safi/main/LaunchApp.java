aa1cfd1740618c1f1365ea3a601e0576fab88af4
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-22 13:56:21 +0530

    feat(bean)- Bean class through base component annotation

--- SpringCoreXML_Annotation/src/main/java/com/safi/main/LaunchApp.java
+++ SpringCoreXML_Annotation/src/main/java/com/safi/main/LaunchApp.java
@@ -1,10 +1,17 @@
 package com.safi.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.safi.config.Password;
+
 public class LaunchApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		ApplicationContext container = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Password ps =container.getBean(Password.class);
+		System.out.println("Password :"+ps.getAlgo());
 	}
 
 }
