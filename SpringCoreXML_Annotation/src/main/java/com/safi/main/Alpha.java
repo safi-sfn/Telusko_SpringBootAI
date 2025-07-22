06cf7d02b56505a4603c61f02a640e7c4b41747c
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-22 13:57:25 +0530

    feat(bean)- alpha class with component annotation

---
+++ SpringCoreXML_Annotation/src/main/java/com/safi/main/Alpha.java
@@ -0,0 +1,12 @@
+package com.safi.main;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alpha {
+
+	public Alpha() {
+		System.out.println("Alpha bean created");
+	}
+
+}
