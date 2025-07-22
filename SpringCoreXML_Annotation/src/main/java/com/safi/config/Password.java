b90b964e1389d36975cfbe4e8c7dee7861df812f
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-22 14:00:02 +0530

    feat - password class for Bean annotation

---
+++ SpringCoreXML_Annotation/src/main/java/com/safi/config/Password.java
@@ -0,0 +1,13 @@
+package com.safi.config;
+
+public class Password {
+	private String algo;
+	public Password(String algo) {
+		this.algo=algo;
+	}
+	
+	public String getAlgo() {
+		return algo;
+	}
+
+}
