1b6f8a9d475b70bfd082f037c58811a8d52aac6e
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-27 11:11:38 +0530

    feat(main): demo StudentDetail with lambda output

--- 03SpringJDBC/src/main/java/com/safi/Application.java
+++ 03SpringJDBC/src/main/java/com/safi/Application.java
@@ -1,13 +1,28 @@
 package com.safi;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.safi.modelLayer.Student;
+import com.safi.repositoryLayer.StudentDetailImpl;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext container = SpringApplication.run(Application.class, args);
+		StudentDetailImpl stDetail = container.getBean(StudentDetailImpl.class);
+		
+//		List<Student> stList = stDetail.getStudentInfo();
+//		for(Student st : stList) {
+//			System.out.println(st);
+//		}
+		
+		// using Lambda expression
+		stDetail.getStudentInfo().forEach(st->System.out.println(st));
 	}
 
 }
