b4d97f09dcec9a1ab8007f5a1016d1bf616b02f5
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-31 18:01:55 +0530

    feat(main) : main class

--- 04SpringDataJDBC/src/main/java/com/safi/Application.java
+++ 04SpringDataJDBC/src/main/java/com/safi/Application.java
@@ -2,12 +2,13 @@ package com.safi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext container = SpringApplication.run(Application.class, args);
 	}
 
 }
